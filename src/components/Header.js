a081dee5c9a9051d458517f0de525ae70d7d70c6
ibrahim-mufti <ibrahimmufti36@gmail.com>
2024-02-05 22:18:16 +0500

    Modernize the header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 import "../App.css"
 import headerImg from '../Assets/Brain.svg'
 function Header () {
+    const brainPlacement = () => {
+        let brainCont = document.getElementById('brain')
+        brainCont.classList.remove('justify-center')
+        brainCont.firstChild.classList.remove('img-shake')
+        brainCont.classList.add('img-animation', 'justify-end')
+        let headCont = document.getElementById('h-text')
+        headCont.classList.remove('opacity-0')
+        headCont.classList.add('header-text-move')
+    }
     return (
-        <div className="container max-w-7xl pt-14 pb-14 mx-auto text-white">
+        <div className="container max-w-7xl py-16 mx-auto text-white">
             <div className="w-full text-lg flex justify-evenly items-center py-14">
-                <div className="font-bold text-center w-1/2">
-                    <h2 className="text-3xl mb-4"><strong>Hello in the World of WD</strong></h2>
-                    <p className="mb-4 text-left">Here you can have all the Information Regarding Html | CSS | JS stay firm and Learn With us</p>
-                    <p className="mb-4 text-left">Here we will Learn About Html/CSS</p>   
+                <div id="h-text" className="font-bold text-[#9b9b9b] text-center w-1/2 opacity-0" >
+                    <h2 className="text-3xl mb-4 relative bottom-12 left-40"><strong>Hello in the World of WD</strong></h2>
+                    <p className="mb-4 text-left relative bottom-[30px] left-4 ">Here you can have all the Information Regarding Html | CSS | JS stay firm and Learn With us</p>
+                    <p className="mb-4 text-left relative top-1 left-4 ">Here we will Learn About Html/CSS</p>   
                 </div>
-                <div className="w-1/2 h-96 flex justify-center">
-                    <img className="img-shadows" src={headerImg}/>
+                <div id="brain" className='w-1/2 h-96 flex justify-center opacity-0 translate-x-[-310px] -z-10'>
+                    <img className="cursor-pointer img-shadows " src={headerImg} onClick={brainPlacement}/>
                 </div>
             </div>
         </div>    
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,20 @@
 import '../App.css';
 import React from 'react';
 import Header from '../components/Header'
+import { useState } from 'react';
 
 function Home () {
+    const [bgFix, setbgFix] = useState(false)
+    let brain = document.getElementById('brain')
+
+    const handleContent = () => {
+        setTimeout(() =>{
+            brain.classList.remove('opacity-0', '-z-10')
+            brain.classList.add('opacity-100', 'transition-opacity', 'duration-300', 'ease-linear', 'z-10')
+        },2500)
+    }
     return(
-        <header className='bg-header'>
+        <header className={`bg-header ${ bgFix ? 'bg-stay' : '' }`} onMouseEnter={handleContent}  onTransitionEnd={() => setbgFix(true) }>
             <Header/>
         </header>
     );
