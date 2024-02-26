c3eec3ac62c470aad2553726e4b14ead91cee115
Ibrahim-mufti <Ibrahimmufti36@gmail.com>
2024-02-26 22:59:20 +0500

    Resolved conflicts

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -8,10 +8,6 @@ function SignIn(props) {
     const passwordRef = useRef()
     const rememberRef = useRef()
 
-    const iconStyle={
-        mixBlendMode:"normal"
-    };
-
     //Methods
 
     const sendActivation = () => {
